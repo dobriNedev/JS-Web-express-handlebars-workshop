21cdcf3758dfdb31d9460beca0946229186a9293
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-01-19 16:55:26 +0100

    details a nd 404 functionality added

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,16 +1,30 @@
 const Cube = require('../models/Cube');
+const db = require('../db.json');
 
 exports.getCreateCubePage = (req, res) => {
     res.render('create');
 };
 
 exports.postCreateCube = (req, res) => {
-    //TO DO save data
     const { name, description, imageUrl, dificultyLevel } = req.body;
     let cube = new Cube(name, description, imageUrl, dificultyLevel);
     cube.save();
-    console.log(req.body);
-    //TO DO redirect
+    
     res.redirect('/');
+};
 
+exports.getDetails = (req, res) => {
+    let cubeId = Number(req.params.cubeId);
+
+    if (!cubeId) {
+        return res.redirect('/404');
+    }
+
+    let cube = db.cubes.find(el => el.id === cubeId);
+
+    if (!cube) {
+        return res.redirect('/404');
+    }
+
+    res.render('details', { cube });
 };
--- src/routes.js
+++ src/routes.js
@@ -7,12 +7,11 @@ const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
 
 router.get('/', homeController.getHomePage);
-
 router.get('/about', homeController.getAboutPage);
+router.get('/404', homeController.getErrorPage);
 
 router.get('/create', cubeController.getCreateCubePage);
-
 router.post('/create', cubeController.postCreateCube);
-
+router.get('/details/:cubeId', cubeController.getDetails);
 
 module.exports = router;
