c062d75099fb12e20f2e1fe99abec3b006b51f71
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-01-19 12:30:14 +0100

    adding post controller and testing functionality

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,3 +1,11 @@
 exports.getCreateCubePage = (req, res) => {
     res.render('create');
 };
+
+exports.postCreateCube = (req, res) => {
+    
+    console.log(req.body);
+    
+    res.send('form submited');
+
+};
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,6 @@ const router = Router();
 //const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
-const aboutController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
 
 router.get('/', homeController.getHomePage);
@@ -12,5 +11,8 @@ router.get('/', homeController.getHomePage);
 router.get('/about', homeController.getAboutPage);
 
 router.get('/create', cubeController.getCreateCubePage);
+
+router.post('/create', cubeController.postCreateCube);
+
 
 module.exports = router;
