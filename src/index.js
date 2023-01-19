8f6405444ae2ac732cb715dc3fcca982f8d392e6
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-01-19 12:21:16 +0100

    adding urlencoded body parser

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ const routes = require('./routes');
 
 const config = require('./config/config.js');
 const viewEngineSetup = require('./config/viewEngineConfig.js');
+const { urlencoded } = require('express');
 
 const app = express();
 
 viewEngineSetup(app);
 
 app.use(express.static('./src/public'));
+
+app.use(express.urlencoded({extended: false})) //we don't need it to be true for our project
 
 app.use(routes);
 
