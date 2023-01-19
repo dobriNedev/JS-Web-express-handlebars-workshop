ec3581691a1835ba606736350cc35531f14a2d20
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-01-19 10:16:00 +0100

    setup dev enviroment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('initial');
+const express = require('express');
+const config = require('./config/config.js');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello!');
+});
+
+app.listen(config.PORT, () => {console.log(`Server is running on port ${config.PORT}...`)});
