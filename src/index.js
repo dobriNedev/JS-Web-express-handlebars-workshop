2ee46f2e25d09d0097b079579e427108dc334e57
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-01-19 11:15:45 +0100

    nav functionality about and create(static) pages

--- src/index.js
+++ src/index.js
@@ -9,10 +9,16 @@ viewEngineSetup(app);
 app.use(express.static('./src/public'));
 
 
-
-
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/create', (req, res) => {
+    res.render('create');
 });
 
 app.listen(config.PORT, () => {console.log(`Server is running on port ${config.PORT}...`)});
