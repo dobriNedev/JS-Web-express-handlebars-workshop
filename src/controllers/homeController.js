3418182851635b50a8e181b15f29d9efb9f389eb
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-01-20 11:27:57 +0100

    bug fixed

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,21 +3,26 @@ const db = require('../db.json');
 exports.getHomePage = (req, res) => {
     const { search, from: difficultyFrom, to: difficultyTo } = req.query;
     
+    let isSearch = false;
+
     let cubes = db.cubes;
     
     if (search) {
         cubes = cubes.filter(el => el.name.toLowerCase().includes(search.toLowerCase()));
+        isSearch = true;
     }
 
     if (difficultyFrom) {
         cubes = cubes.filter(el => el.difficultyLevel >= difficultyFrom);
+        isSearch = true;
     }
 
     if(difficultyTo) {
         cubes = cubes.filter(el => el.difficultyLevel <= difficultyTo);
+        isSearch = true;
     }
      
-    res.render('index', { cubes, search, difficultyFrom, difficultyTo });
+    res.render('index', { cubes, search, difficultyFrom, difficultyTo, isSearch});
 };
 
 exports.getAboutPage = (req, res) => {
