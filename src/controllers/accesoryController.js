015954c659e0725e30528fb72cdbc39f749f2b94
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-01-27 17:39:54 +0100

    added POST functionality to accesory controller

--- src/controllers/accesoryController.js
+++ src/controllers/accesoryController.js
@@ -4,3 +4,13 @@ exports.getAccessoryCreate = async(req, res) => {
     let accessories = await Accessory.find().lean();
     res.render('createAccessory', { accessories });
 };
+
+exports.postAccessoryCreate = async(req, res) => {
+    const { name, description, imageUrl } = req.body;
+
+    const accessory = new Accessory({ name, description, imageUrl });
+
+    await Accessory.save();
+
+    res.redirect('/');
+};
