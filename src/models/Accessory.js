7a51bd7fbafd380aa7eae6f3bc1dab9129369053
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-01-29 15:21:21 +0100

    added http/https validation for accessoty imageUrl

--- src/models/Accessory.js
+++ src/models/Accessory.js
@@ -8,6 +8,13 @@ const accessoryShema = new mongoose.Schema({
     imageUrl: {
         type:String,
         required: true,
+        //http/https validation with validate
+        validate: {
+            validator: function(value){//value = url input
+                return value.startsWith('http://') || value.startsWith('https://')
+            },
+            message: 'Invalid URL!'//error message
+        }
     },
     description: {
         type: String,
